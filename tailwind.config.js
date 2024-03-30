b54241a6de095e0b23271dfa9e11ef6524737897
meat <155785205+meatmeatmeatmeatmeatmeat@users.noreply.github.com>
2024-03-30 16:07:19 +1000

    Fixed up tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,36 +4,37 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      colors: {
-        primary: '#282c34',
-        secondary: '#191f27',
-        highlight: '#eaebf3',
-        outline: '#c5c6d1',
-      },
-      gridTemplateColumns: {
-        player: '122px minmax(100px, 1fr) max-content minmax(0px, 60px)',
-        playersm: '120px minmax(100px, 1fr) 18px',
-        scoreboardnav: '114px minmax(95px, 1fr) 55px',
-        scoreboardnavsm: '180px 130px',
-        scoreboardgrid: '1fr 3px 1fr',
-        scoreboardgridsm: '1fr',
-        playerdetails: '150px auto',
-        playerdetailscontent: '160px 3px auto',
-      },
-      gridTemplateRows: {
-        playerhistorycard: '40px 22px 40px auto',
-      },
-      fontFamily: {
-        build: ['"Build"', 'sans-serif'],
-      },
+    purge: {
+        content: ['./public/**/*.html', './src/**/*.{js,jsx,ts,tsx}'],
     },
-    screens: {
-      xs: '475px',
-      ...defaultTheme.screens,
+    theme: {
+        extend: {
+            colors: {
+                primary: '#282c34',
+                secondary: '#191f27',
+                highlight: '#eaebf3',
+                outline: '#c5c6d1',
+            },
+            gridTemplateColumns: {
+                player: '122px minmax(100px, 1fr) max-content minmax(0px, 60px)',
+                playersm: '120px minmax(100px, 1fr) 18px',
+                scoreboardnav: '114px minmax(95px, 1fr) 55px',
+                scoreboardnavsm: '180px 130px',
+                scoreboardgrid: '1fr 3px 1fr',
+                scoreboardgridsm: '1fr',
+                playerdetails: '150px auto',
+                playerdetailscontent: '160px 3px auto',
+            },
+            gridTemplateRows: {
+                playerhistorycard: '40px 22px 40px auto',
+            },
+            fontFamily: {
+                build: ['"Build"', 'sans-serif'],
+            },
+        },
+        screens: {
+            xs: '475px',
+            ...defaultTheme.screens,
+        },
     },
-  },
-  plugins: [],
 };
