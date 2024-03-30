2551e0ed47598fe3e66c525e65955a7a3d699dea
meat <155785205+meatmeatmeatmeatmeatmeat@users.noreply.github.com>
2024-03-30 15:10:03 +1000

    Added modalContext from MAC, various app functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,90 @@
 import './App.css'
 import Dashboard from "./pages/dashboard/dashboard.tsx";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {setLanguage} from "../i18n";
+import {getAllSettings} from "./preferences.ts";
+import {useModal} from "./context/ModalContext.tsx";
 
 function App() {
     const [currentPage, setCurrentPage] = useState('/');
+    const {closeModal, openModal, modalContent} = useModal();
     const renderPage = () => {
         switch (currentPage) {
             case '/':
-
+                return <Dashboard/>;
             default:
                 return <Dashboard/>;
         }
     };
+    const isBackendConnected = async () => {
+        try {
+            // const backendRunning = await verifyBackend();
+            // if (!backendRunning) throw new Error('Backend not running');
+            return true;
+        } catch (e) {
+            console.error('Error verifying backend connection', e);
+            // Close any previous modal
+            closeModal();
+            // openModal(<CantConnectModal/>);
+            return false;
+        }
+    };
+    const verifyConfigured = async () => {
+        try {
+            // const configured = await isBackendConfigured();
+            // if (!configured) throw new Error('Backend not configured');
+            if (modalContent) closeModal();
+            return true;
+        } catch (e) {
+            console.error('Error verifying backend configuration', e);
+            // Close any previous modal
+            closeModal();
+            // openModal(<ConfigurationModal closeModal={closeModal} />);
+            return false;
+        }
+    };
+    const verificationRoutine = async () => {
+        let connected = false;
+        do {
+            connected = await isBackendConnected();
+            if (!connected) {
+                await new Promise((resolve) => setTimeout(resolve, 5000)); // Wait 5 seconds before retrying
+            }
+        } while (!connected);
+        void verifyConfigured();
+    };
+    useEffect(() => {
+        // Set language from settings
+        const setLanguageFromSettings = async () => {
+            const settings = await getAllSettings();
+            if (settings.external.language) {
+                setLanguage(settings.external.language);
+            }
+        };
+        void setLanguageFromSettings();
+
+        // Don't verify backend if we're using fakedata (dev environment)
+        // if (useFakedata) return;
+
+        void verificationRoutine();
+        const intervalId = setInterval(verificationRoutine, 5000);
+
+        return () => {
+            clearInterval(intervalId);
+        };
+    }, [currentPage]);
     return (
-        <div>
-            {renderPage()}
+        <div className="App">
+            {/*<Modal/>*/}
+            {/*{!isMinimode && (*/}
+            {/*    <div className="App-sidebar">*/}
+            {/*        /!*<SideMenu setCurrentPage={setCurrentPage} currentPage={currentPage}/>*!/*/}
+            {/*    </div>*/}
+            {/*)}*/}
+            <div className="App-content">
+                {/*<ContentPageContainer>{renderPage()}</ContentPageContainer>*/}
+                {renderPage()}
+            </div>
         </div>
     )
 }
