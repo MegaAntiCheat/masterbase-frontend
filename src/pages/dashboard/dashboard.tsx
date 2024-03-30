dd4ba750287e8e4f20840b92d1caaf00d3bf221b
meat <155785205+meatmeatmeatmeatmeatmeat@users.noreply.github.com>
2024-03-30 14:44:44 +1000

    Added tailwind & css from MAC, tf2 font, updated npm scripts with tailwind cmd

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,5 +1,13 @@
+import {t} from "../../../i18n";
+
 function Dashboard() {
-    return <h1>meat</h1>
+    // return
+    return (
+        <div className="">
+            <h1>meat</h1>
+            <div>{t('PREFERENCES')}</div>
+        </div>
+    );
 }
 
 export default Dashboard
