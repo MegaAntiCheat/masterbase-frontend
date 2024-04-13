19ec006365be6d5a504ccf44eba1ec5bf3d68b1a
meat <155785205+meatmeatmeatmeatmeatmeat@users.noreply.github.com>
2024-04-13 18:42:09 +1000

    Modal

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import {useMinimode} from "./context/MinimodeContext.tsx";
 import SideMenu from "./components/SideMenu/SideMenu.tsx";
 import {PAGES} from "./constants/menuConstants.tsx";
 import ContentPageContainer from "./components/ContentPageContainer/ContentPageContainer.tsx";
+import { Modal } from './components/Modal/Modal.tsx';
 
 function App() {
     const {isMinimode} = useMinimode();
@@ -81,6 +82,7 @@ function App() {
     }, [currentPage]);
     return (
         <div className="App">
+            <Modal />
             {!isMinimode && (
                 <div className="App-sidebar">
                     <SideMenu setCurrentPage={setCurrentPage} currentPage={currentPage}/>
---
+++ src/components/Modal/Modal.tsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import Modal from 'react-modal';
+import './Modal.css';
+import ReactModal from 'react-modal';
+import { useModal } from '../../context/ModalContext';
+
+interface ModalOptions {
+  dismissable?: boolean;
+}
+
+interface ModalProps {
+  title?: string | React.ReactNode;
+  footer?: React.ReactNode;
+  show?: boolean;
+  children?: React.ReactNode;
+  className?: string;
+  onClose?: () => void;
+  modalOptions?: ModalOptions;
+}
+
+Modal.setAppElement('#root');
+
+const ModalWrapper = ({ className, onClose }: ModalProps) => {
+  const ModalRef = React.useRef<HTMLDivElement>(null);
+  const { closeModal, modalContent, modalOptions } = useModal();
+
+  const handleClickOutside = (e: MouseEvent) => {
+    if (ModalRef.current && !ModalRef.current.contains(e.target as Node)) {
+      onClose?.();
+    }
+  };
+
+  React.useEffect(() => {
+    if (modalOptions?.dismissable) {
+      document.addEventListener('mousedown', handleClickOutside);
+
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }
+  }, []);
+
+  return (
+    <>
+      <ReactModal
+        isOpen={!!modalContent}
+        className={`modal ${className}`}
+        overlayClassName="modal-overlay"
+        ariaHideApp={false}
+        onRequestClose={modalOptions?.dismissable ? closeModal : void 0}
+      >
+        <div ref={ModalRef}>{modalContent}</div>
+      </ReactModal>
+    </>
+  );
+};
+
+export { ModalWrapper as Modal };
