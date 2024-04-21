39801b0468f52d6d5f4252f1df9710ff1d1ea985
meat <155785205+meatmeatmeatmeatmeatmeat@users.noreply.github.com>
2024-04-21 20:49:56 +1000

    Removed sidebar, updated page container layout, Updated menuHeader with option to hide action

--- src/components/ContentPageContainer/ContentPageContainer.tsx
+++ src/components/ContentPageContainer/ContentPageContainer.tsx
@@ -1,14 +1,28 @@
 import {ReactNode} from 'react';
 import './ContentPageContainer.css';
 import TextItem from "../Text/TextItem.tsx";
+import MenuHeader from "../SideMenu/MenuHeader.tsx";
 
 interface ContentPageProps {
-    children: ReactNode;
+    children: ReactNode,
+    online: boolean
 }
 
-const ContentPageContainer = ({children}: ContentPageProps) => {
+const ContentPageContainer = ({children, online}: ContentPageProps) => {
     return <div className="content-page">
-        <TextItem className="page-header text-3xl font-bold my-6" fontSize="h1">Masterbase</TextItem>
+        <div style={{display: 'flex', justifyContent: 'space-between'}}>
+            <div style={{display: 'flex'}}>
+                <MenuHeader
+                    collapsed={true}
+                    handleSymbolClick={() => {
+                    }}
+                    hideIcon={true}
+                />
+                <TextItem className="page-header text-3xl font-bold ml-6 my-6" fontSize="h1">Masterbase</TextItem>
+            </div>
+            <TextItem className="page-header text-3xl font-bold my-6"
+                      fontSize="h1">{online ? 'online' : 'offline'}</TextItem>
+        </div>
         {children}
     </div>;
 };
