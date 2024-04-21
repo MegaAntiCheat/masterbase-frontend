97072fe028d64ac8fdf1b6a4a8893bec5fb08b58
meat <155785205+meatmeatmeatmeatmeatmeat@users.noreply.github.com>
2024-04-21 20:39:09 +1000

    Updated api handler

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,22 @@
 const backendUrl = 'http://localhost:8080/api/'; // proxy
 // const backendUrl = 'http://localhost:8000/'; // original
 
-async function fetchMasterbase(endpoint: string, body?: BodyInit) {
-    const data = await fetch(`${backendUrl}${endpoint}`, {'Content-Type': 'application/json', body});
+async function fetchMasterbase<T>(endpoint: string, body?: BodyInit, options: RequestInit = {
+    headers: {'Content-Type': 'application/json'},
+    body
+}): Promise<T | string> {
+    options.headers = new Headers({
+        'Content-Type': 'application/json',
+        ...(options.headers || {})
+    });
+    options.body = body;
+    const data: Response = await fetch(`${backendUrl}${endpoint}`, options);
+    if (options.headers.get('Content-Type') === 'text/plain') return await data.text();
     return await data.json();
+}
+
+export function status() {
+    return fetchMasterbase('', undefined, {headers: {'Content-Type': 'text/plain'}});
 }
 
 export function sessionId(api_key: string, demo_name: string, fake_ip: string, map: string) {
@@ -11,15 +24,15 @@ export function sessionId(api_key: string, demo_name: string, fake_ip: string, m
 }
 
 export function closeSession(api_key: string) {
-    return fetch(`${backendUrl}close_session?api_key=${api_key}`);
+    return fetchMasterbase(`close_session?api_key=${api_key}`);
 }
 
 export function provision(location?: string) {
-    return fetch(`${backendUrl}provision?location=${location}`);
+    return fetchMasterbase(`provision?location=${location}`);
 }
 
 export function provisionHandler() {
-    return fetch(`${backendUrl}provision_handler`);
+    return fetchMasterbase(`provision_handler`);
 }
 
 export function provisionUrl() {
@@ -27,13 +40,13 @@ export function provisionUrl() {
 }
 
 export function lateBytes(api_key: string, body: BodyInit) {
-    return fetch(`${backendUrl}late_bytes?api_key=${api_key}`, {method: 'POST', body});
+    return fetchMasterbase(`late_bytes?api_key=${api_key}`, body, {method: 'POST'});
 }
 
 export function demoData(api_key: string, session_id: string) {
-    return fetch(`${backendUrl}demodata?api_key=${api_key}&session_id=${session_id}`);
+    return fetchMasterbase(`demodata?api_key=${api_key}&session_id=${session_id}`);
 }
 
 export function listDemos(api_key: string, page_size?: number, page_number?: number) {
-    return fetch(`${backendUrl}list_demos?api_key=${api_key}&page_size=${page_size}&page_number=${page_number}`);
+    return fetchMasterbase(`list_demos?api_key=${api_key}&page_size=${page_size}&page_number=${page_number}`);
 }
